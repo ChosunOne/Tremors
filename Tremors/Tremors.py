96a89be8a05d0163e1ae956094274786b9ef64c0
Josiah Evans <koreanhalo@gmail.com>
2016-11-01 19:27:42 -0500

    Computes tremor zones and graphs them by long/lat

--- Tremors/Tremors.py
+++ Tremors/Tremors.py
@@ -1,7 +1,9 @@
 import glob
 import numpy as np
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.cm as cm
 import pandas as pd
 import os 
 import datetime as dt
@@ -18,10 +20,6 @@ depths = []
 magnitudes = []
 types = []
 
-# Line from Kumamoto to Osaka
-geoLine = analysis.Line(32.8031, 130.7079, 34.6937, 135.5022)
-
-
 # For debug, just do the following number of lines
 LINECOUNT = 30000
 
@@ -32,7 +30,13 @@ for file in glob.glob('JMA*.txt'):
             if count < LINECOUNT:
                 if line[0] == '#':
                     continue
+
                 line = line.split()
+                
+                #Check if in bounding box
+                if float(line[2]) < 26 or float(line[3]) < 123 or float(line[2]) > 48 or float(line[3]) > 154:
+                    continue
+                
                 dates += [line[0] + " " + line[1][:-3]]
                 latitudes += [float(line[2])]
                 longitudes += [float(line[3])]
@@ -62,26 +66,70 @@ depths = np.array(depths)
 magnitudes = np.array(magnitudes)
 types = np.array(types)
 
+xd = np.linspace(125, 153, 7)
+yinterp = interp1d(longitudes, latitudes)
+
+geoLines = []
+for x in xd:
+    if x != xd[-1]:
+        x1 = x  
+        y1 = yinterp(x1).tolist()
+        x2 = xd[np.where(xd==x1)[0][0] + 1]
+        y2 = yinterp(x2).tolist()
+        geoLine = analysis.Line(x1, y1, x2, y2)
+        geoLines += [geoLine]
+
 # Calculated Data
-distances = []
+distances = [[] for x in range(0, len(geoLines))]
+eventDates = [[] for x in range(0, len(geoLines))]
+eventLatitudes = [[] for x in range(0, len(geoLines))]
+eventLongitudes = [[] for x in range(0, len(geoLines))]
+eventDepths = [[] for x in range(0, len(geoLines))]
+eventTypes = [[] for x in range(0, len(geoLines))]
+eventMagnitudes = [[] for x in range(0, len(geoLines))]
+inf = np.inf
+
 for i in range(0, len(latitudes)):
-    d = geoLine.distance(latitudes[i], longitudes[i])
-    
-    # Remove extreme outliers that are not in the area of interest
-    if d > 10 or d < -10:
-        d = 0
-    distances += [d]
+    shortestDistance = inf
+    closestLine = 0
+    for l in geoLines:
+        dist = l.distance(longitudes[i], latitudes[i])
+        if abs(dist) < abs(shortestDistance):
+            shortestDistance = dist
+            closestLine = geoLines.index(l)
 
-distanceSeries = pd.Series(distances, dates)
-distancesRollingMean = distanceSeries.rolling(center=False, window=1400).mean()
-distancesExpRollingMean = distanceSeries.ewm(span=1400).mean()
+    if abs(shortestDistance) < 5:
+        distances[closestLine] += [shortestDistance]
+        eventDates[closestLine] += [dates[i]]
+        eventLatitudes[closestLine] += [latitudes[i]]
+        eventLongitudes[closestLine] += [longitudes[i]]
+        eventDepths[closestLine] += [depths[i]]
+        eventTypes[closestLine] += [types[i]]
+        eventMagnitudes[closestLine] += [magnitudes[i]]
 
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d %H:%M:%S'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('Tremor Zones')
+ax.set_xlabel('Longitude (°)')
+ax.set_ylabel('Latitude (°)')
 
-#plt.scatter(dates, distances)
-#plt.scatter(dates, distancesRollingMean)
-plt.scatter(dates, distancesExpRollingMean)
+#distanceKTSeries = pd.Series(distancesKT, dates)
+#distancesKTRollingMean = distanceKTSeries.rolling(center=False, window=1400).mean()
+#distancesKTExpRollingMean = distanceKTSeries.ewm(span=1400).mean()
 
-plt.gcf().autofmt_xdate()
+#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d %H:%M:%S'))
+#plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
+
+#plt.scatter(dates, distancesKT)
+#plt.scatter(dates, distancesKTRollingMean)
+#plt.scatter(dates, distancesKTExpRollingMean)
+#plt.scatter(longitudes, latitudes)
+#plt.scatter(eventLongitudes[1], eventLatitudes[1])
+colors = cm.rainbow(np.linspace(0, 1, len(geoLines)))
+for l in range(0, len(geoLines)):
+    ax.scatter(eventLongitudes[l], eventLatitudes[l], color = colors[l])
+
+ax.plot(xd, yinterp(xd), color='k', linestyle='-', linewidth=3)
+
+#plt.gcf().autofmt_xdate()
 plt.show()
--- Tremors/modules/analysis.py
+++ Tremors/modules/analysis.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+from scipy import optimize
+import numpy as np
 
 class Line:
     def __init__(self, x1, y1, x2, y2):
@@ -15,9 +17,29 @@ class Line:
         x2 = self.x2
         y1 = self.y1
         y2 = self.y2
-        num = (y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1
-        denom = ((y2 - y1)**2 + (x2 - x1)**2)**.5
-        return num / denom
+
+        px = x2-x1
+        py = y2-y1
+
+        something = px*px + py*py
+        u =  ((x0 - x1) * px + (y0 - y1) * py) / float(something)
+
+        if u > 1:
+            u = 1
+        elif u < 0:
+            u = 0
+
+        x = x1 + u * px
+        y = y1 + u * py
+
+        dx = x - x0
+        dy = y - y0
+        dist = (dx*dx + dy*dy)**.5
+        return dist
         
-        
+def piecewise_linear(x, x0, y0, k1, k2):
+    return np.piecewise(x, [x < x0], [lambda x:k1*x + y0-k1*x0, lambda x:k2*x + y0-k2*x0])
     
+def fitLines(xList, yList):
+    p, e = optimize.curve_fit(piecewise_linear, xList, yList)
+    return p
